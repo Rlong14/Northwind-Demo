04b835ea6af21c10d399307b4c9c89d026c82ee1
Rlong14 <rlong14@nait.ca>
2017-09-26 11:51:56 -0600

    Added Product Category web page

---
+++ NorthwindData.App/DTOs/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindData.App.DTOs
+{
+    public class ProductCategory
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+        public IEnumerable<ProductInfo> Products { get; set; }
+    }
+
+}
